6f1183024d339b9daaf85c18e591cbc07ed5a3b9
subugee <sasubramanian_md@hotmail.com>
2025-12-18 12:24:25 +0530

    setup

---
+++ springapps/appembededtomcat/src/main/java/com/example/AppembededtomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppembededtomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppembededtomcatApplication.class, args);
+	}
+
+}
---
+++ springapps/appexternaltomcat/src/main/java/com/example/AppexternaltomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppexternaltomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppexternaltomcatApplication.class, args);
+	}
+
+}
